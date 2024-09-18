9a9d2429fa18a5496df3595d57abb73f0813141e
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2024-09-18 12:54:50 +0200

    simple form, config and seed and fixtures

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,9 +16,9 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice "Quote was successfully created."
+      redirect_to quotes_path, notice: "Quote was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,7 +29,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully updated."
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
